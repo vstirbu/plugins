010c69624fe4e6d23b51736031e8e959cb4b285d
Vlad Stirbu <vlad.stirbu@nokia.com>
2017-05-10 15:17:35 +0300

    added test

--- test/bodyReader.js
+++ test/bodyReader.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // core requires
+var http = require('http');
 
 // external requires
 var assert = require('chai').assert;
@@ -18,6 +19,55 @@ var PORT;
 
 
 describe('body reader', function () {
+
+    describe('jsons as text', function () {
+        beforeEach(function (done) {
+            SERVER = restify.createServer({
+                dtrace: helper.dtrace,
+                log: helper.getLog('server')
+            });
+
+            SERVER.listen(0, '127.0.0.1', function () {
+                PORT = SERVER.address().port;
+
+                done();
+            });
+        });
+
+        afterEach(function (done) {
+            SERVER.close(done);
+        });
+
+        it('decode application/*+json content as text', function (done) {
+            var data = 'my test representation';
+
+            SERVER.use(plugins.bodyParser());
+
+            SERVER.post('/jsons/custom-media-type', function (req, res, next) {
+                assert.deepEqual(req.body, data);
+                res.send();
+                next();
+            });
+
+            var opts = {
+                hostname: '127.0.0.1',
+                port: PORT,
+                path: '/jsons/custom-media-type',
+                agent: false,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/vnd.my.own.type+json'
+                }
+            };
+
+            var client = http.request(opts, function (res) {
+                assert.equal(res.statusCode, 200);
+                done();
+            });
+            client.write(data);
+            client.end();
+        });
+    });
 
     describe('gzip content encoding', function () {
 
